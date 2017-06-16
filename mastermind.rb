75e764e1677ec172ffeb200d2221cc09a24556ec
David Moylan <davecm010@gmail.com>
2017-06-16 09:13:16 -0500

    another commit

--- mastermind.rb
+++ mastermind.rb
@@ -4,8 +4,10 @@ class Mastermind
   def initialize
     @user_guess = []
     @winning_combo = nil
-    @color_options = "|red|green|yellow|blue|orange|purple|black|white|"
+    @color_array = "|red|green|yellow|blue|orange|purple|black|white|"
+    @color_choices = ['red', 'green', 'yellow', 'blue', 'orange', 'purple', 'black', 'white']
     @turn_count = 0
+    @hints = nil
   end
 
   def new_game
@@ -16,7 +18,9 @@ class Mastermind
       @turn_count += 1
       get_answer
       display_answer
-      end_round_hint
+      end_round
+      show_hints
+      game_over?
       color_array_display
     end
   end
@@ -29,20 +33,19 @@ class Mastermind
     puts ""
     puts "Enter in 4 of the following colors in any order:"
     puts ""
-    puts @color_options
+    puts @color_array
     puts ""
   end
 
   def color_array_display
     if @turn_count % 3 == 0 && @turn_count > 1
-      puts @color_options
+      puts @color_array
       puts ""
     end
   end
 
   def combo_randomizer
-    color_choices = ['red', 'green', 'yellow', 'blue', 'orange', 'purple', 'black', 'white']
-    @winning_combo = color_choices.sample(4)
+    @winning_combo = @color_choices.sample(4)
   end
 
   def get_answer
@@ -50,6 +53,11 @@ class Mastermind
     for i in 1..4
       print "#{i}:"
       input = gets.chomp.downcase.strip
+      until @color_choices.include? input
+        puts "Must include a color from the array!
+        print "#{i}:"
+        input = gets.chomp.downcase.strip
+      end
       @user_guess << input
     end
     puts ""
@@ -64,11 +72,43 @@ class Mastermind
     puts ""
   end
 
-  def end_round_hint
-    2.times do
-      puts "|_|_|"
+  def end_round
+    @hints = []
+    @user_guess.each_with_index do |color, i|
+      if @winning_combo[i] == color
+        @hints.unshift("[x]")
+      elsif @winning_combo.include? color
+        @hints << "[o]"
+      end
     end
-    puts ""
+  end
+
+  def show_hints
+    if @hints.empty?
+      2.times do
+        puts "[ ][ ]"
+      end
+    else
+      i = 0
+      2.times do
+        for x in i..i+1
+          print @hints[x]
+        end
+        i += 2
+        puts""
+      end
+    end
+    puts ''
+  end
+
+  def game_over?
+    if @user_guess == @winning_combo
+      puts "You figured out the secret code, you win!"
+      exit
+    elsif @turn_count >= 12
+      puts "You have run out of guesses, GAME OVER..."
+      exit
+    end
   end
 
 end
