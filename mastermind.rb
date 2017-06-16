4b38c9e27ca105231ab371189eb64b7a9ef1d993
David Moylan <davecm010@gmail.com>
2017-06-16 12:02:16 -0500

    created a second scenario where the user creates a sequence for the computer to guess

--- mastermind.rb
+++ mastermind.rb
@@ -1,6 +1,5 @@
 class Mastermind
 
-
   def initialize
     @user_guess = []
     @winning_combo = nil
@@ -8,22 +7,19 @@ class Mastermind
     @color_choices = ['red', 'green', 'yellow', 'blue', 'orange', 'purple', 'black', 'white']
     @turn_count = 0
     @hints = nil
+    @game = nil
   end
 
   def new_game
     title_sequence
-    color_array_display
-    combo_randomizer
-    12.times do
-      @turn_count += 1
-      get_answer
-      display_answer
-      end_round
-      show_hints
-      game_over?
-      color_array_display
+    if @game == '1'
+      guessing_game
+    elsif @game == '2'
+      create_array
     end
   end
+
+  private
 
   def title_sequence
     puts ""
@@ -31,10 +27,67 @@ class Mastermind
     puts ""
     puts "|_?_|_?_|_?_|_?_|"
     puts ""
+    instructions
+    game_type
+  end
+
+  def instructions
+    puts "Instructions? (Y/N)"
+    answer = gets.chomp.strip.downcase
+    if answer == 'y'
+      puts ""
+      puts "Your objective is to guess the hidden sequence of 4 random colors."
+      puts "Not only must you guess the correct 4 colors, but their order as well."
+      puts "No single color is used more than once."
+      puts "You have 12 rounds to guess the sequence."
+      puts "At the end of each round, you will be given hints to help deduce the right sequence."
+      puts "An [x] denotes you have correctly guessed one of the colors as well as its position in the sequence."
+      puts "An [o] denotes you have correctly guessed one of the colors, but its order is incorrect"
+      gets.chomp
+    end
+  end
+
+  def game_type
+    puts ""
+    puts "Would you like to guess the computer's secret array,"
+    puts "or challenge the computer with an array of your OWN?"
+    puts "Type: '1' to GUESS hidden array | Type: '2' to CREATE an array"
+    @game = gets.chomp.strip
+  end
+
+  def guessing_game #initializes option '1' where user guesses computer generated sequence
     puts "Enter in 4 of the following colors in any order:"
     puts ""
     puts @color_array
     puts ""
+    combo_randomizer
+    12.times do
+      @turn_count += 1
+      get_answer
+      display_answer
+      end_round
+      show_hints
+      user_game_over?
+      color_array_display
+    end
+  end
+
+  def create_array
+    puts "Put 4 of the following colors in any sequence:"
+    puts ""
+    puts @color_array
+    puts ""
+    get_answer #uses get_answer to create winning_combo
+    @winning_combo = @user_guess
+    12.times do
+      @turn_count += 1
+      computer_guess
+      display_answer
+      end_round
+      show_hints
+      computer_game_over?
+      sleep 2
+    end
   end
 
   def color_array_display
@@ -46,6 +99,10 @@ class Mastermind
 
   def combo_randomizer
     @winning_combo = @color_choices.sample(4)
+  end
+
+  def computer_guess
+    @user_guess = @color_choices.sample(4)
   end
 
   def get_answer
@@ -64,6 +121,7 @@ class Mastermind
       end
       @user_guess << input
     end
+    @user_guess
     puts ""
   end
 
@@ -105,12 +163,22 @@ class Mastermind
     puts ''
   end
 
-  def game_over?
+  def user_game_over?
     if @user_guess == @winning_combo
       puts "You figured out the secret code, you win!"
       exit
     elsif @turn_count >= 12
       puts "You have run out of guesses, GAME OVER..."
+      exit
+    end
+  end
+
+  def computer_game_over?
+    if @user_guess == @winning_combo
+      puts "the computer figured out the secret code, GAME OVER..."
+      exit
+    elsif @turn_count >= 12
+      puts "The computer ran out of guesses, YOU WIN!"
       exit
     end
   end
